a0f0f60ab0b05cd23b87f5a3d5ded13f2cacc735
ProfessorKnicks <ProfessorKnicks@users.noreply.github.com>
2025-01-21 21:57:07 +0000

    deploy: 6207bd47676c54e51a29de37ba64f8401ac21a0b

---
+++ precache-manifest.2005d6024617a97f01117a24596935b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2252ce7580d04e67bfe635e410111e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c9def33a1abc4844109",
+    "url": "/static/css/main.66f1a1da.chunk.css"
+  },
+  {
+    "revision": "8ce5c11268e80ab4a468",
+    "url": "/static/js/2.ce748eb0.chunk.js"
+  },
+  {
+    "revision": "9c9def33a1abc4844109",
+    "url": "/static/js/main.47d2ff16.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d4cf0ad46512f79b71642d11e85a5da8",
+    "url": "/static/media/HomePage.d4cf0ad4.png"
+  }
+]);
