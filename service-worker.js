88846ce6d1487abda90fda09ccf480108cb15e55
ProfessorKnicks <ProfessorKnicks@users.noreply.github.com>
2025-01-21 22:10:27 +0000

    deploy: 2e096962eaed815ed2d2bf2b3998296e06858f87

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2005d6024617a97f01117a24596935b4.js"
+  "/cyberpattern.github.io/precache-manifest.fe9df610711ca2f5596ac2f7259e8201.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cyberpattern.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
