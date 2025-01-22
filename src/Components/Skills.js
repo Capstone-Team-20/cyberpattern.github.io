235bfd7a08c5c58f217456639403d9cdbdea47ec
pmahesh29 <pmahesh@uark.edu>
2025-01-22 10:20:10 -0600

    Editing Skills Page

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -4,16 +4,14 @@ import "../Styles/RegistrationPage.css";
 const RegistrationPage = () => {
   const [skills, setSkills] = useState(5);
 
-  const passwordMatch = (password, verifyPassword) => {
-    return password === verifyPassword;
-  }
+
   
   return (
     <div className="registration-container">
       <div className="registration-content">
         <h1>Please Enter Your Details:</h1>
         <form>
-        <label>Which Tools Do you want to use?</label>
+        <label><u>Which Tools Do you want to use?</u></label>
           <div className="tools-section">
             <div className="tool-option">
               <input
@@ -57,14 +55,51 @@ const RegistrationPage = () => {
             </div>
           </div>
 
-          <label>Password</label>
-          <input type="password" required />
+{/* Virtual Machines */}
 
-          <label>Verify Password</label>
-          <input type="password" required />
-          
-          <label>Date of Birth</label>
-          <input type="date" required />
+          <label>Which Virtual Machines do you want to use?</label>
+          <div className="tools-section">
+            <div className="tool-option">
+              <input
+                type="checkbox"
+                name="tools"
+                value="CTF"
+                checked={skills.CTF}
+                onChange={() => setSkills({ ...skills, CTF: !skills.CTF })}
+              />
+              <span>Kali Linux</span>
+            </div>
+            <div className="tool-option">
+              <input
+                type="checkbox"
+                name="tools"
+                value="Kali Linux"
+                checked={skills["Kali Linux"]}
+                onChange={() =>
+                  setSkills({
+                    ...skills,
+                    "Vulnerability Simulation": !skills["Vulnerability Simulation"],
+                  })
+                }
+              />
+              <span>Vulnerability Simulation</span>
+            </div>
+            <div className="tool-option">
+              <input
+                type="checkbox"
+                name="tools"
+                value="Penetration Testing"
+                checked={skills["Penetration Testing"]}
+                onChange={() =>
+                  setSkills({
+                    ...skills,
+                    "Penetration Testing": !skills["Penetration Testing"],
+                  })
+                }
+              />
+              <span>Penetration Testing</span>
+            </div>
+          </div>
 
           <label>What would you rate your skills?</label>
           <input
