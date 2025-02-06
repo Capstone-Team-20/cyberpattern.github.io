914aec4f7eb1c3766a72bc99d4875ceb027ecb96
ProfessorKnicks <charlie.d.poolman@gmail.com>
2025-02-06 14:42:51 -0600

    Update LoginPage.js

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -4,9 +4,8 @@ import image from "../Assets/HomePage.png"; // Adjust the path as needed
 import { useNavigate } from 'react-router-dom';  // Make sure to import useNavigate
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseURL = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
-
+const supabase_url = "https://kdzamdxnnnzodftvjcrh.supabase.co";
+const supabase_anon = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkemFtZHhubm56b2RmdHZqY3JoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc2NzA5NzIsImV4cCI6MjA1MzI0Njk3Mn0.0Ml4p6x7VDY2m5_t2ISl0aEYpEum-vD8uFL1BYxBaes";
 const supabase = createClient(supabaseURL, supabaseAnonKey);
 
 const LoginPage = () => {
