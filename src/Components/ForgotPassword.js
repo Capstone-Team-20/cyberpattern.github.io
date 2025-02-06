d16502dc840873370c27379a6016876852194bf5
ProfessorKnicks <charlie.d.poolman@gmail.com>
2025-02-06 14:57:05 -0600

    Update ForgotPassword.js

--- src/Components/ForgotPassword.js
+++ src/Components/ForgotPassword.js
@@ -4,9 +4,8 @@ import image from "../Assets/HomePage.png";
 import { useNavigate } from 'react-router-dom';
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseURL = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
-
+const supabaseURL = "https://kdzamdxnnnzodftvjcrh.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkemFtZHhubm56b2RmdHZqY3JoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc2NzA5NzIsImV4cCI6MjA1MzI0Njk3Mn0.0Ml4p6x7VDY2m5_t2ISl0aEYpEum-vD8uFL1BYxBaes";
 const supabase = createClient(supabaseURL, supabaseAnonKey);
 
 const ForgotPassword = () => {
