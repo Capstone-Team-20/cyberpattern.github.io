99d664db437e121034d86db4f384da4244ee5ec4
Tyler Cash <148647245+tcash1990@users.noreply.github.com>
2025-03-12 13:24:45 -0500

    fixed vm url
    The VM url wasnt working, changed it, works locally, hopefully it will work for production.

--- src/Components/VMPage.js
+++ src/Components/VMPage.js
@@ -4,7 +4,7 @@ import React from 'react';
 const VMPage = ({ viewOnly, shouldReconnect, password }) => {
   // Construct the URL for the iframe directly
   //const vncUrl = `http://192.168.10.141:6080/vnc.html?autoconnect=true&reconnect=${shouldReconnect}&view_only=${viewOnly}&password=${password}`;
-  const vncUrl2 = `https://72.209.113.80:443/vnc.html?autoconnect=true&reconnect=${shouldReconnect}&view_only=${viewOnly}&password=${password}`;
+  const vncUrl2 = `http://72.209.113.80:443/vnc.html?autoconnect=true&reconnect=${shouldReconnect}&view_only=${viewOnly}&password=${password}`;
   return (
     <div>
       <h1>Virtual Machine Access</h1>
