0e90126be6a93f4b795db0361082d90d662213d9
ProfessorKnicks <charlie.d.poolman@gmail.com>
2025-04-03 20:46:28 -0500

    Update ForgotPassword.js

--- src/Components/ForgotPassword.js
+++ src/Components/ForgotPassword.js
@@ -4,9 +4,11 @@ import { createClient } from "@supabase/supabase-js";
 import "../Styles/Setup.css";  // Apply the same styles as RegistrationPage
 import logo from "../Assets/Logo.png"; // Import the logo image
 
+// Access environment variables
+const supabaseURL = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
 // Initialize Supabase Client
-const supabaseURL = "https://kdzamdxnnnzodftvjcrh.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkemFtZHhubm56b2RmdHZqY3JoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc2NzA5NzIsImV4cCI6MjA1MzI0Njk3Mn0.0Ml4p6x7VDY2m5_t2ISl0aEYpEum-vD8uFL1BYxBaes";
 const supabase = createClient(supabaseURL, supabaseAnonKey);
 
 const ForgotPassword = () => {
